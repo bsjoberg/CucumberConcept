c040259c4e64ddfdce8f786c605b5daac021a2b6
Brian Sjoberg <brian_sjoberg@yahoo.com>
2015-11-07 14:22:41 -0500

    Rearranged some packages and got the feature file to work.

--- CucumberConcept/src/test/java/com/bdd/CucumberConcept/CucumberRunner.java
+++ CucumberConcept/src/test/java/com/bdd/CucumberConcept/CucumberRunner.java
@@ -1,4 +1,4 @@
-package cucumber;
+package com.bdd.CucumberConcept;
 
 import org.junit.runner.RunWith;
 
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
-		features = "src/test/cucumber"
+		features = "src/test/resources"
 		)
 public class CucumberRunner {
 
