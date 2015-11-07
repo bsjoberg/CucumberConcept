18c77aecc32fbaa8c64141c204c2f08d78fa06c0
Brian Sjoberg <brian_sjoberg@yahoo.com>
2015-11-07 14:25:15 -0500

    Initial steps file.

---
+++ CucumberConcept/src/test/java/com/bdd/CucumberConcept/CucumberConceptSteps.java
@@ -0,0 +1,26 @@
+package com.bdd.CucumberConcept;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CucumberConceptSteps {
+	@Given("^this is my first step$")
+	public void this_is_my_first_step() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^this is my second step$")
+	public void this_is_my_second_step() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^this is my final step$")
+	public void this_is_my_final_step() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
